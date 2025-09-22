bc077b03acf27bb9ede0b1441c4334658a6d01f8
Paige Wilson-Coleman <pwilson-coleman@catalyte.io>
2025-09-22 14:52:24 -0500

    did one part of function

--- requirments.py
+++ requirments.py
@@ -1,10 +1,12 @@
 {
 "quizName": "string",
 "quizModule": "string",
-"quizScore": number,
-"studentId": number,
+"quizScore": int,
+"studentId": int,
 "studentName": "string",
 "submissionDate": "string"
 }
 
-def filter_by__date():
+def filter_by_date(due_date, list_of_submissions):
+    if not list_of_submissions:
+        return []
